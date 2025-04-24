deb5e80965ad40a93858ba233b46770409b37699
Joram Wayne <joramwmuya@gmail.com>
2025-04-24 19:08:23 +0300

    Complete lab

---
+++ src/components/data.js
@@ -0,0 +1,12 @@
+const CATEGORIES = ["All", "Code", "Food", "Money", "Misc"];
+
+const TASKS = [
+  { text: "Buy rice", category: "Food" },
+  { text: "Save a tenner", category: "Money" },
+  { text: "Build a todo app", category: "Code" },
+  { text: "Build another todo app", category: "Code" },
+  { text: "Take a nap", category: "Misc" },
+];
+
+export { CATEGORIES, TASKS };
+
